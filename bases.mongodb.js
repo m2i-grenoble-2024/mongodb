bcd9d2b5689582d9eca1429aa73ee9aa8e4943d1
Jean Demel <demeljean@hotmail.fr>
2024-06-18 11:53:02 +0200

    queries done

--- bases.mongodb.js
+++ bases.mongodb.js
@@ -37,20 +37,33 @@ db.person.deleteMany({firstName:'Roger'})
 db.person.findOne(new ObjectId('667042e81e078566dc19d150'))
 // db.person.updateMany({}, {age: 30})
 
-db.person.find();
-
-
-
-
-
-
 /**
  * Requêtes à faire sur nos persons : 
  * 1. Avec un find récupérer toutes les persons qui sont plus de 50 ans
  * 2. Récupérer les persons qui ont comme prénom Jaylyn ou Amari
- * 3. Récupérer les persons qui possèdent le skill PHP
+ * 3. Récupérer les persons qui possèdent le skill PHP, et faire aussi une requête
+ * pour récupérer les personnes qui ont UNIQUEMENT le skill PHP
  * 4. Récupérer les persons qui ont plus de 4 skills
  * 5. Récupérer les persons dont le prénom termine par un a
  * 6. Faire un update pour incrémenter de 1 l'âge des personnes qui ont comme
  * name 'Sommer'
  */
+
+db.person.find({
+    age: {$gte: 50}
+});
+
+db.person.find({
+    firstName:{
+        $in: ['JayLin', 'Amari']
+    }
+});
+
+db.person.find({skills:'PHP'});//Personnes qui ont PHP dans leurs skills
+db.person.find({skills:['PHP']}); //Personnes qui ont QUE PHP comme skills
+
+db.person.find({'skills.3': {$exists:true}});
+
+db.person.find({firstName:/a$/});
+
+db.person.updateMany({name:'Sommer'}, {$inc:{age:1}});
